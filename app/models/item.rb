67e503aeb956fedef78728fe0b514e46823bd3a0
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-06-03 15:08:52 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :send_day
+
   belongs_to :user
   has_one_attached :image
-end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :send_day_id
+  end
+
+
+  with_options presence: true do
+    validates :name
+    validates :information
+    validates :image
+    validates :price, numericality: { 
+      only_integer: true, 
+      greater_than_or_equal_to: 300, 
+      less_than_or_equal_to: 9999999
+    }
+  end
+
