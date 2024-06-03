cc4139414b0d2308f372c604df2ed6825fa3c623
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-06-03 10:52:14 +0900

    Rubocopの実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,21 +1,21 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation{password}
-    first_name {Faker::Japanese::Name.first_name}
-    first_name_kana {generate_katakana}
-    last_name {Faker::Japanese::Name.last_name}
-    last_name_kana {generate_katakana}
-    birthday {Faker::Date.backward(days: 365 * 25)}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name { Faker::Japanese::Name.first_name }
+    first_name_kana { generate_katakana }
+    last_name { Faker::Japanese::Name.last_name }
+    last_name_kana { generate_katakana }
+    birthday { Faker::Date.backward(days: 365 * 25) }
   end
 end
 
 def generate_katakana
-  katakana = ""
+  katakana = ''
   rand(3..7).times do
-    katakana << [*"ァ".."ン"].sample
+    katakana << [*'ァ'..'ン'].sample
   end
   katakana
 end
