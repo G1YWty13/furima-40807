815ca2b10f567b58ce55c61a820b5e8ef85ddb33
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-06-04 17:52:05 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    return if current_user.id == @item.user.id
+
+    redirect_to action: :index
   end
 
   def update
