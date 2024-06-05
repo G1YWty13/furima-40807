6a440f81334469b12347d4f909e6a43b23ba8aa6
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-06-05 14:31:27 +0900

    ルーティングとコントローラーの記述

---
+++ app/controllers/deals_controller.rb
@@ -0,0 +1,22 @@
+class DealsController < ApplicationController
+
+  def index
+    @deal_address = DealAddress.new
+  end
+
+  def create
+    @deal_address = DealAddress.new(deal_params)
+    if @deal_address.valid?
+      @deal_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def deal_params
+    params.require(:deal_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :price).merge(user_id: current_user.id)
+  end
+  
+end
