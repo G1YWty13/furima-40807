b889d29bec71d2590e3bf70b20c0a06e095c0d95
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-06-05 16:44:06 +0900

    ActiveHashの値の許可

--- app/models/deal_address.rb
+++ app/models/deal_address.rb
@@ -1,10 +1,10 @@
 class DealAddress
   include ActiveModel::Model
-  attr_accessor :housenumber, :prefecture, :town, :street, :building, :phone, :user_id, :item_id
+  attr_accessor :housenumber, :prefecture_id, :town, :street, :building, :phone, :user_id, :item_id
 
   with_options presence: true do
     validates :housenumber, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :town
     validates :street
     validates :phone, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Include hyphen(-)"}
